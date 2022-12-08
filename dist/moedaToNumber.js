3241606a13070dee3b90ad8a7d06f2c6fb63fab2
fescarvalho <fernando_scarvalho@hotmail.com>
2022-12-08 20:14:09 -0300

    first commit

--- dist/moedaToNumber.js
+++
@@ -1,6 +0,0 @@
-export default function moedaToNumber(moeda) {
-    const numero = Number(moeda.replaceAll(".", "").replace(",", "."));
-    console.log(numero);
-    return isNaN(numero) ? null : numero;
-}
-//# sourceMappingURL=moedaToNumber.js.map
